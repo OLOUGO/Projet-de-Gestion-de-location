cb21946134453cb4c129dc836d27b32f59e2e59d
Noubiyoyo <geoffroyolougo@gmail.com>
2022-11-06 23:07:02 +0100

    init

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -75,7 +75,7 @@
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
 
             <a href="index3.html" class="brand-link">
-                <span class="brand-text font-weight-bold " style="font-size:1.3em;"> Adeno Location </span>
+                <span class="brand-text font-weight-bold" style="font-size:1.3em;"> Adeno Location </span>
             </a>
 
             <div class="sidebar">
